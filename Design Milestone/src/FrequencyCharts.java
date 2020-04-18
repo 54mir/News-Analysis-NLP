1a4ef15ff3cff3da147dff38e0838f4bd1431d36
Samir Patel <skpatel@seas.upenn.edu>
2020-04-18 11:14:36 -0500

    Reduced the number of people displayed on getTopPerson chart

--- Design Milestone/src/FrequencyCharts.java
+++ Design Milestone/src/FrequencyCharts.java
@@ -4,6 +4,7 @@ import org.knowm.xchart.XYChartBuilder;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class FrequencyCharts extends CommonMethods{
@@ -16,13 +17,32 @@ public class FrequencyCharts extends CommonMethods{
         int span = years * 12 + months + 1;
         HashMap<String, int[]> personMentions = new HashMap<>();
 
+        //List of only people mentioned at least X number of times in data set
+        int atLeastXTimes = 5;
+        HashMap<String, Integer> mostMentioned = new HashMap<>();
+        HashMap<String, Integer> shortList = new HashMap<>();
+        for (Article article : super.articles) {
+//        Consumer<String> = article.getTopPerson();
+            if (article.getTopPerson() != null && mostMentioned.containsKey(article.getTopPerson())){
+                Integer x = mostMentioned.get(article.getTopPerson()) + 1;
+                mostMentioned.put(article.getTopPerson(), x);
+            } else {
+                mostMentioned.put(article.getTopPerson(), 1);
+            }
+        }
+        for (String key : mostMentioned.keySet()) {
+            if (mostMentioned.get(key) >= atLeastXTimes){
+                shortList.put(key, mostMentioned.get(key));
+            }
+        }
+
         int counter = 0;
         for (LocalDate date = super.firstDate; date.isBefore(super.lastDate); date = date.plusMonths(1)) {
             dateSeries.add(super.convertDate(date));
             for (Article article : super.articles) {
                 if (article.getDate().equals(date) || (article.getDate().isAfter(date) && article.getDate().isBefore(date.plusMonths(1)))){
 
-                    if (article.getTopPerson() != null) {
+                    if (article.getTopPerson() != null  && shortList.containsKey(article.getTopPerson())) {
                         if (personMentions.containsKey(article.getTopPerson())){
                             personMentions.get(article.getTopPerson())[counter] += 1;
 
@@ -38,7 +58,8 @@ public class FrequencyCharts extends CommonMethods{
         }
 
         // Create Chart
-        org.knowm.xchart.XYChart chart = new XYChartBuilder().width(800).height(600).title("Number of Articles about a person over Time").xAxisTitle("X").yAxisTitle("Y").build();
+        org.knowm.xchart.XYChart chart = new XYChartBuilder().width(800).height(600).
+                title("Number of Articles about a person over Time").xAxisTitle("X").yAxisTitle("Y").build();
 
         //Add Series
         for (String name : personMentions.keySet()) {
